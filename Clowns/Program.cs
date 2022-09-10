7eab70f2943d8cf6bb5f6a250099eee079365b34
osaja <osaja@DELLVOSTRO>
2022-09-10 19:43:38 +0200

    Add project files.

---
+++ Clowns/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clowns
+{
+    class Program
+    {
+        class TallGuy: IClown
+        {
+            public string Name;
+            public int Height;
+
+            public void TalkAboutYourself()
+            {
+                Console.WriteLine($"My name is {Name} and I'm {Height} inches tall.");
+            }
+
+            public string FunnyThingIHave
+            {
+                get { return "big shoes"; }
+            }
+
+            public void Honk()
+            {
+                Console.WriteLine("Honk honk!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            TallGuy tallGuy = new TallGuy() { Height = 76, Name = "Jimmy" };
+            tallGuy.TalkAboutYourself();
+            Console.WriteLine($"The tall guy has {tallGuy.FunnyThingIHave}");
+            tallGuy.Honk();
+        }
+    }
+}
